e92809a3b8e0dc25fbfaf9033964e3afda96e4a3
Jeff Horner <jeff@stratic.io>
2020-08-12 18:49:51 +0100

    chore: Remove unneeded argument

--- crawl.js
+++ crawl.js
@@ -5,7 +5,6 @@ import {Fetcher} from "./src/Fetcher"
 const startPage = process.argv[2] || 'bbc.co.uk'
 const searchToken = process.argv[3] || 'Renault'
 const parallelism = process.argv[4] || 4
-const maxDepth = process.argv[5] || 10
 
 const domain = startPage.match(/^(https?:\/\/)?([^/]*).*$/)[2]
 console.log(`Domain: ${domain}`)
